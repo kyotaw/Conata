8771403b6140e961dbfb1fb06347071b64de9dbe
kyotaw <kyotaw@users.noreply.github.com>
2015-01-28 00:48:26 +0900

    tool to query wikipedia about area info

--- tool/query_area_info.py
+++ tool/query_area_info.py
@@ -16,33 +16,55 @@ if len(sys.argv) > 1:
 
 area_root = path.get_area_root()
 scraped_text_folder = 'topics'
-ignore_folders = [scraped_text_folder]
+ignore_folders = [scraped_text_folder, '１丁目', '２丁目', '３丁目']
 
-err_file = codecs.open('query_error', 'w')
+log_file = codecs.open('wiki_query.log', 'w')
+
+def extract_characteristic_area_name(delim, area_name):
+    query = ''
+    if delim in area_name:
+        tokens = area_name.split(delim)
+        if len(tokens) >= 2:
+            query = delim.join(tokens[1:])
+        else:
+            query = tokens[0] + delim 
+    return query
+
+def get_query_word(area_name):
+    query = extract_characteristic_area_name('郡', area_name)
+    if query != '':
+        return query
+    query = extract_characteristic_area_name('町', area_name)
+    if query != '':
+        return query
+    query = extract_characteristic_area_name('大字', area_name)
+    if query != '':
+        return query
+    return area_name
 
 for dir_path, sub_dirs, file_names in os.walk(area_root):
     dir_name = os.path.basename(dir_path)
     if dir_name in ignore_folders:
+        log_file.write('skip ' + dir_name + '\n') 
         continue
-    
+   
     folder_path = '/'.join([dir_path, scraped_text_folder])
     file_path = '/'.join([folder_path, 'wiki_' + dir_name + '.txt'])
     if not overwrite and os.path.exists(file_path):
         print(dir_name + ' already queried')
         continue
+  
+    query = get_query_word(dir_name)
    
-    query = ''
-    tokens = dir_name.split('郡')
-    if len(tokens) > 2:
-        err_file.write("invalid city name :" + dir_name + '\n')
-    elif len(tokens) == 2:
-       query = tokens[1]
-    else:
-        query = dir_name
+    log = dir_name + ' talk to wikipedia using keyword : ' + query
+    print(log)
+    log_file.write(log + '\n')
 
-    print('query info for : ' + query)
     xml = query_wikipedia(dir_name)
-    text = scrap_wiki(xml)
+    if xml != '':
+        text = scrap_wiki(xml)
+    else:
+        log_file.write('wiki page for ' + dir_name + ' not found\n') 
 
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
